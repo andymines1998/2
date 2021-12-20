05ae229239e95b6f16e7ef54fa8fcf52c4afe5fa
andymines1998 <96443641+andymines1998@users.noreply.github.com>
2021-12-20 22:21:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+let addMessage = document.querySelector('.message');
+let addButton = document.querySelector('.add');
+let todo = document.querySelector('.todo')
+let todoList = []
+if(localStorage.getItem('todo')){
+    todoList = JSON.parse(localStorage.getItem('todo'));
+    displayMessages();
+}
+
+
+addButton.addEventListener('click', function(){
+    let newTodo = {
+        todo: addMessage.value,
+        checked: false,
+        important: false 
+    };
+    console.log(newTodo)
+
+    todoList.push(newTodo)
+    displayMessages();
+    localStorage.setItem('todo' , JSON.stringify(todoList))
+
+})
+
+function displayMessages (){
+    let displayMessage = ''
+    todoList.forEach(function(item, i){
+        displayMessage += `
+        <li>
+        <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
+        <label for='item_${i}' class="${item.important ? 'important' : ''}">${item.todo}</label>
+        
+
+
+        </li>`;
+        todo.innerHTML = displayMessage;
+        
+
+    })
+}
+
+todo.addEventListener('change', function(event){
+    let valueLabel = todo.querySelector('[for='+ event.target.getAttribute('id') + ']');
+    todoList.forEach(function(item){
+        if (item.todo === valueLabel){
+            item.checked = !item.checked;
+            localStorage.setItem('todo' , JSON.stringify(todoList));
+        }
+    });
+});
+
+todo.addEventListener('contextmenu', function(event){
+    event.preventDefault();
+    todoList.forEach(function(item, i){
+        if(item.todo === event.target.innerHTML){
+            if(event.ctrlKey || metaKey){
+                todoList.splice(i, 1);
+            }else{
+                item.important = !item.important;
+
+            }
+           
+            
+            displayMessage();
+            localStorage.setItem('todo' , JSON.stringify(todoList));
+            
+        }
+    });
+})
